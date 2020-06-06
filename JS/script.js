dbf9e8c6da8e1186acc2f186e54cc093e9ceeb87
Michael Beebower <michael.beebower@g.austincc.edu>
2020-06-06 09:21:02 -0500

    Updated script.js with img markup with srcset and sizes attributes

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,11 @@
 const IMAGES = document.querySelectorAll("img");    // find all of the images in the document
+const SIZES = {                                             // define constant mapping for the image types and their associated width values
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px"
+};
+
 
 function makeSrcSet(imgSrc) {
     let markup = [];    // initialize markup to empty array
@@ -17,8 +24,11 @@ for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");     // get the src path for each image
     imgSrc = imgSrc.slice(0, -8);                   // strip off the last 8 characters of the src path for each image
     let srcSet = makeSrcSet(imgSrc);                // call the makeSrcSet function
-    console.log(srcSet);                            // display comma-separated markup list
+    IMAGES[i].setAttribute("srcset", srcSet);
+    // console.log(srcSet);                            // display comma-separated markup list
 
     let type = IMAGES[i].getAttribute("data-type");     // get data-type attribute of each image
-    console.log(type);
+    let sizes = SIZES[type];                        // set sizes using the data-type attribute and the associated value of the SIZES dictionary / mapping
+    IMAGES[i].setAttribute("sizes", sizes);
+    // console.log(sizes);
 }
